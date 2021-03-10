1d54cba67063b73d2caf97ee7ed1a568a5f93ef1
elizarov017 <ftuti60@gmail.com>
2021-03-10 02:47:57 +0200

    1.5.5
    готова 5 і половина 6 дз

--- server.js
+++ server.js
@@ -1,9 +1,2 @@
-// var livereload = require('livereload');
-
-const express = require('express');
-const app = express();
-
-app.use(express.static('./'));
-app.listen(3005, () => console.log("good"));
-
-
+var main = require('./Backend/main');
+main.startServer(5050);
